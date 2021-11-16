6068e260a5b3eb8ba007d2826736c9a635394b25
Gabryel Einstein de Carvalho Amaro <gabryel808einstein@gmail.com>
2021-11-16 11:31:46 -0300

    Criando funcao que calcula imc

--- Módulo 3/44. exercício de massa corporal/Modelo/assets/js/mai.js
+++ Módulo 3/44. exercício de massa corporal/Modelo/assets/js/mai.js
@@ -1,20 +1,42 @@
 //Capturar envento se submit do fomulário
 const form = document.querySelector('#formulario')
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     const inputPeso = event.target.querySelector('#peso');
     const inputAltura = event.target.querySelector('#altura');
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
-    console.log (peso, altura)
+
+    if (!peso) {
+        setResultado("peso invalido", false);
+        return;
+    }
+    if (!altura) {
+        setResultado('altura invalida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    console.log(imc)
 
 });
-function criaP (className)
-{ const p = document.createElement('p');
-return p;
+
+function getNivelImc (imc){
+    const nivel = ['abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
 }
-function setResultado (msg){
+function getImc (peso, altura){
+    const imc = peso / altura **2;
+    return imc.toFixed(2);
+}
+
+function criaP(className) {
+    const p = document.createElement('p');
+    return p;
+}
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
     const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
