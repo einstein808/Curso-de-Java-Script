1c35bac0ec61672534604c9f23ae52708e321a23
Gabryel Einstein de Carvalho Amaro <gabryel808einstein@gmail.com>
2021-11-16 10:40:00 -0300

    aprendendo sobre querySelector

--- Módulo 3/44. exercício de massa corporal/Modelo/assets/js/mai.js
+++ Módulo 3/44. exercício de massa corporal/Modelo/assets/js/mai.js
@@ -3,5 +3,19 @@ const form = document.querySelector('#formulario')
 form.addEventListener('submit', function(event){
     event.preventDefault();
     console.log('evento previnido');
+    setResultado('olamundo')
 
 });
+function criaP ()
+{ 
+    
+}
+function setResultado (msg){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado');
+    p.innerHTML= 'qualquer coisa';
+    resultado.appendChild(p);
+}
